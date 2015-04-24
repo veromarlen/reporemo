f4cf719995d8c116ac0615ca10d4cef137369401
mjorgesantos <santosjorge882@gmail.com>
2015-04-24 13:18:12 -0500

    .03 post con objetos

--- reeditgam/site/scripts/app.js
+++ reeditgam/site/scripts/app.js
@@ -4,9 +4,17 @@ console.log("cargo app.js");
 -angular.module("reeditgam",[]);
 +var modulo1 =angular.module("reeditgam",[]);
  modulo1.controller("mainCtrl",[
+
  	'$scope',
  	function($scope){
  		$scope.test = "Hola angular";
-+		$scope.post=["post1","post2","post3","post4","post5","post6"];
-+
+-		$scope.post=["post1","post2","post3","post4","post5","post6"];
+-
++		$scope.post=["{Title: "post1",upvotes:5}",
++		"{title: "post2", upvotes: 15}",
++		"{title: "post3", upvotes:  7}",
++		"{title: "post4", upvotes: 9}",
++		"{title: "post5",  upvotes: 3}",
++		"{title: "post6", upvotes: 5}"];
++		
  }]); 
