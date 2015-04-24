f209460a2757151a82d56fa1d1a02475b25c3d40
mjorgesantos <santosjorge882@gmail.com>
2015-04-24 13:08:44 -0500

    .02 Post con ng-Repeat

--- reeditgam/site/scripts/app.js
+++ reeditgam/site/scripts/app.js
@@ -1,9 +1,12 @@
 console.log("cargo app.js");
-//<!--parametros-->
-var modulo1 =
-angular.module("reeditgam",[]);
-modulo1.controller("mainCtrl",[
-	'$scope',
-	function($scope){
-		$scope.test = "Hola angular";
-}]); 
+ //<!--parametros-->
+-var modulo1 =
+-angular.module("reeditgam",[]);
++var modulo1 =angular.module("reeditgam",[]);
+ modulo1.controller("mainCtrl",[
+ 	'$scope',
+ 	function($scope){
+ 		$scope.test = "Hola angular";
++		$scope.post=["post1","post2","post3","post4","post5","post6"];
++
+ }]); 
