d12297f0bb5c98977cf61f278d377b7aed53718f
mjorgesantos <santosjorge882@gmail.com>
2015-04-24 13:27:11 -0500

    .04 agregando nuevo post staticos

--- reeditgam/site/scripts/app.js
+++ reeditgam/site/scripts/app.js
@@ -1,20 +1,28 @@
 console.log("cargo app.js");
  //<!--parametros-->
--var modulo1 =
--angular.module("reeditgam",[]);
-+var modulo1 =angular.module("reeditgam",[]);
+var modulo1 =angular.module("reeditgam",[]);
+var modulo1 =
+angular.module("reeditgam",[]);
  modulo1.controller("mainCtrl",[
-
  	'$scope',
  	function($scope){
  		$scope.test = "Hola angular";
--		$scope.post=["post1","post2","post3","post4","post5","post6"];
--
-+		$scope.post=["{Title: "post1",upvotes:5}",
-+		"{title: "post2", upvotes: 15}",
-+		"{title: "post3", upvotes:  7}",
-+		"{title: "post4", upvotes: 9}",
-+		"{title: "post5",  upvotes: 3}",
-+		"{title: "post6", upvotes: 5}"];
-+		
+		$scope.post=["{Title: "post1",upvotes:5}",
+		"{title: "post2", upvotes: 15}",
+		"{title: "post3", upvotes:  7}",
+		"{title: "post4", upvotes: 9}",
+		"{title: "post5",  upvotes: 3}",
+		"{title: "post6", upvotes: 5}"];
+		$scope.posts=[{title: "post1",upvotes:5},
+		{title: "post2", upvotes: 15},
+		{title: "post3", upvotes:  7},
+		{title: "post4", upvotes: 9},
+		{title: "post5",  upvotes: 3},
+		{title: "post6", upvotes: 5}];
+		//metodo de controlador 
+		%scope.addPost=function(){
+			$scope.post.push(
+				{title:"New Post", upvotes:0});
+		};
+ 		
  }]); 
